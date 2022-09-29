1cb0512b97ca43fcddf530410ce5734b6ef0d9cc
Matthew Boakes <Matthew.Boakes@Gmail.com>
2022-09-29 13:20:40 +0100

    Retrieved Model

--- catdog_segmentation_model/model.py
+++ catdog_segmentation_model/model.py
@@ -1,26 +1,28 @@
 import os
+
+import numpy as np
 import requests
 import torch
 from torchvision import transforms
-import numpy as np
+
 import unet
 
 
 class CatDogUNet:
     def __init__(self):
         filename = "unet_model.ckpt"
-        ### TODO ###
-        ### Download the file containing the weights of the pre-trained model from url
-        ### if the file doesn't exist already locally, and write it to a file.
-        ### After that, create an instance of the model class.
-        ### The code to load the model weights from file and evaluate the model is
-        ### already provided.
+        if not os.path.exists(filename):
+            url = "https://connectionsworkshop.blob.core.windows.net/pets/conv_net_model_mse10.ckpt"
+            r = requests.get(url)
+            with open(filename, 'wb') as f:
+                f.write(r.content)
+        self.model = unet.UNet()
         self.model.load_state_dict(torch.load(filename))
         self.model.eval()
 
     def predict(self, image):
         # pre-process input image (as required by model)
-        transform_input = transforms.Compose([transforms.Resize((192, 192)),])
+        transform_input = transforms.Compose([transforms.Resize((192, 192)), ])
         image = image.values
         image = image[:, :, 0:3]  # make sure we have only 3 channels
         image = np.transpose(image, (2, 0, 1))
@@ -34,3 +36,5 @@ class CatDogUNet:
         ### Return the segmentation mask.
         return "FIX ME"
 
+
+CatDogUNet()
